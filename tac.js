4142f3302fd788d2ccc55f708437cb8eb8ced84a
ZainW <zainthegreatcb@gmail.com>
2015-11-27 13:27:23 -0500

    draw X based on canvas mouse up coordinates

--- tac.js
+++ tac.js
@@ -5,10 +5,13 @@
 // else {
 // 	//place O
 // }
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var mouseX, mouseY;
+// c.addEventListener("mouseUp", mouseUp, false);
 $(document).on("ready", function(){
 	function drawgrid(){
-		var c = document.getElementById("myCanvas");
-		var ctx = c.getContext("2d");
+		ctx.beginPath();
 		ctx.moveTo(0,250);
 		ctx.lineTo(750,250);
 		ctx.moveTo(0,500);
@@ -19,7 +22,26 @@ $(document).on("ready", function(){
 		ctx.lineTo(500,750);
 		ctx.stroke();
 	}
+	function drawX(x,y){
+		ctx.beginPath();
+    
+    ctx.moveTo(x - 50, y - 50);
+    ctx.lineTo(x + 50, y + 50);
+    ctx.stroke();
+
+    ctx.moveTo(x + 50, y - 50);
+    ctx.lineTo(x - 50, y + 50);
+    ctx.stroke();
+
+	}
 	drawgrid();
+	$("#myCanvas").mouseup(function(e){
+		 mouseX =  e.offsetX;
+		 mouseY = e.offsetY ;
+		drawX(mouseX,mouseY);
+	});
+	// //test
+	// drawX(100,100);
 
 
 
