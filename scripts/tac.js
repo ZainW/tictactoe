283861e3aab227509876ad54714aac1affd62407
ZainW <zainthegreatcb@gmail.com>
2015-11-29 13:09:42 -0500

    now vader vs Luke instead of X vs O

--- scripts/tac.js
+++ scripts/tac.js
@@ -4,6 +4,14 @@ var ctx = c.getContext("2d");
 var mouseX, mouseY;
 var player1 = true;
 var board = [[],[],[]];
+//images 
+var img1 = new Image();
+var img2 = new Image();
+img1.src = 'icons/vader.png'
+img2.src = 'icons/luke.png'
+
+
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 $(document).on("ready", function(){
   var clickcount = 0;
 	function drawgrid(){
@@ -29,7 +37,43 @@ $(document).on("ready", function(){
 	    ctx.lineTo(x - 50, y + 50);
 	    ctx.stroke();
 	}
-	function drawO(x,y){
+  var circleDefaults = {
+        circlePos: {
+            x: 338,
+            y: 130
+        },
+        radius: 120,
+        counterClockwise: false,
+        startAngle: Math.PI / 2,
+        endAngle: Math.PI * 2,
+        currentPercent: 0,
+        endPercent: 100
+
+    }
+	function drawO(x,y, current){
+    // //internet
+    
+
+    //         ctx.beginPath();
+    //         ctx.arc(x, y, circleDefaults.radius, -(circleDefaults.startAngle), ((circleDefaults.endAngle) * current) - circleDefaults.startAngle, circleDefaults.counterClockwise);
+    //         ctx.lineWidth = 1.0;
+
+    //         ctx.strokeStyle = "#000"
+    //         ctx.stroke();
+    //         ctx.closePath();
+
+    //         circleDefaults.currentPercent++;
+    //         if (circleDefaults.currentPercent < circleDefaults.endPercent) {
+    //             requestAnimationFrame(function () {
+    //                 drawO(circleDefaults.currentPercent / 100);
+    //             });
+    //         }
+    //         // else{
+    //         //     AnimateLine();
+    //         // }
+
+
+    // //mine
 		ctx.beginPath();
 		ctx.arc(x,y,50,0,Math.PI * 2)
 		ctx.stroke();
@@ -92,26 +136,32 @@ $(document).on("ready", function(){
 		mouseY = e.offsetY ;
     
 		var drawsquare = whatSquare(mouseX,mouseY);
-		var x = ((drawsquare[0] -1 ) * 250) + 125;
-		var y = ((drawsquare[1] -1 ) * 250) + 125;
+		// var x = ((drawsquare[0] -1 ) * 250) + 125;
+		// var y = ((drawsquare[1] -1 ) * 250) + 125;
+    var x = ((drawsquare[0] -1 ) * 250);
+    var y = ((drawsquare[1] -1 ) * 250);
     if (clickcount == 9){
       $("#win").fadeIn(3000);
       $("#win h3").text("It's a Draw :s")
     }
 		if (player1 == true){
 			board[drawsquare[0]-1][drawsquare[1]-1] = 'X';
-			drawX(x,y);
+      
+        ctx.drawImage(img1,x,y,250,250);
+      
+			//drawX(x,y);
 			if(isWinnerX() == true){
 				$("#win").fadeIn(3000);
-      	$("#win h3").text("X wins! Refresh to play again.")
+      	$("#win h3").text("Vader wins! Refresh to play again.")
 			}
 		}
 		else {
 			board[drawsquare[0]-1] [drawsquare[1]-1] = 'O';
-			drawO(x,y);
+			//drawO(x,y);
+      ctx.drawImage(img2,x,y,250,250);
 			if (isWinnerO() == true) {
         $("#win").fadeIn(3000);
-        $("#win h3").text("O wins! Refresh to play again.");
+        $("#win h3").text("Luke wins! Refresh to play again.");
 			};
 		}
     
